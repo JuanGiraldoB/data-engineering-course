10b2eb2b1e7346ddbcd9a7dfc9c327576a8d67ab
JuanGiraldoB <jugiraldob@unal.edu.co>
2024-04-29 17:24:35 -0500

    5 day

--- de_zoomcamp/module_1/2_docker_sql/upload_data.py
+++ de_zoomcamp/module_1/2_docker_sql/upload_data.py
@@ -1,34 +1,89 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from time import time
+import argparse
+import argparse
+import os
+import sys
+import pyarrow.parquet as pq
 
-df = pd.read_csv('yellow_tripdata_2021-01.csv', nrows=100)
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-engine.connect()
 
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
 
-df_iter = pd.read_csv('yellow_tripdata_2021-01.csv',
-                      iterator=True, chunksize=100_000)
+    # Get the name of the file from url
+    file_name = url.rsplit('/', 1)[-1].strip()
+    print(f'Downloading {file_name} ...')
+    # Download file from url
+    os.system(f'curl {url.strip()} -o {file_name}')
+    print('\n')
 
-df = next(df_iter)
+    # Read file based on csv or parquet
+    if '.csv' in file_name:
+        df = pd.read_csv(file_name, nrows=10)
+        df_iter = pd.read_csv(file_name, iterator=True, chunksize=100000)
+    elif '.parquet' in file_name:
+        file = pq.ParquetFile(file_name)
+        df = next(file.iter_batches(batch_size=10)).to_pandas()
+        df_iter = file.iter_batches(batch_size=100000)
+    else:
+        print('Error. Only .csv or .parquet files allowed.')
+        sys.exit()
 
-len(df)
+    engine = create_engine(
+        f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    # Create the table
+    df.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
 
-df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-df.head(n=0).to_sql(con=engine, name='yellow_taxi_data', if_exists='replace')
-df.to_sql(con=engine, name='yellow_taxi_data', if_exists='append')
-
-while True:
+    # Insert values
     t_start = time()
+    count = 0
+    for batch in df_iter:
+        count += 1
 
-    df = next(df_iter)
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-    df.to_sql(con=engine, name='yellow_taxi_data', if_exists='append')
+        if '.parquet' in file_name:
+            batch_df = batch.to_pandas()
+        else:
+            batch_df = batch
+
+        print(f'inserting batch {count}...')
+
+        b_start = time()
+        batch_df.to_sql(name=table_name, con=engine, if_exists='append')
+        b_end = time()
+
+        print(f'inserted! time taken {b_end-b_start:10.3f} seconds.\n')
 
     t_end = time()
+    print(
+        f'Completed! Total time taken was {t_end-t_start:10.3f} seconds for {count} batches.')
 
-    print('inserted another chunk..., took %.3f seconds' % (t_end - t_start))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='What the program does',
+    )
+    # user
+    # password
+    # host
+    # port
+    # database name
+    # table name
+    # url of the csv
+
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the postgres table')
+    parser.add_argument('--url', help='url of the csv file')
+
+    args = parser.parse_args()
+    main(args)
